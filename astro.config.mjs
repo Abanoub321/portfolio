30c05986f217a76d5fd94ac5544e227931c4d010
Abanoub321 <48459005+Abanoub321@users.noreply.github.com>
2023-12-16 13:31:15 +0200

    Add Storyblok integration and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,24 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+	integrations: [tailwind(), mdx(), image(), storyblok({
+		accessToken: env.STORYBLOK_TOKEN,
+		components: {
+			// Add your components here
+			blogPost: 'storyblok/BlogPost',
+			blogPostList: 'storyblok/BlogPostList',
+			page: 'storyblok/Page',
+		},
+		apiOptions: {
+			// Choose your Storyblok space region
+			region: 'eu', // optional,  or 'eu' (default)
+		},
+	})],
 });
